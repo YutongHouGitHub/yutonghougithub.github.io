723e745e78c724c4ab04ae47666779dd41db36e5
Yutong HOU <hyt2023@stu.xjtu.edu.cn>
2026-05-12 17:54:48 +0200

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'awards', 'experience', 'publications', 'academic', 'media'];
+const section_names = ['home', 'education', 'awards', 'experience', 'publications', 'academic', 'media'];
 
 
 
